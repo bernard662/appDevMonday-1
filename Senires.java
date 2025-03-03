edfd6131f9594a809188c97ef248d44a205255c7
bernard662 <bcsenires@paterostechnologicalcollege.edu.ph>
2025-03-03 19:38:29 +0800

    Senires.java

---
+++ Senires.java
@@ -0,0 +1,9 @@
+public class Senires {
+    public static void display() {
+        int age = 20; // Replace with user input if necessary
+        System.out.println("Hello! This is Senires contributing to the project.");
+        System.out.println("My current age is: 20 " + age);
+        System.out.println("Next year, I will be: 21 " + (age + 1));
+    }
+}
+
